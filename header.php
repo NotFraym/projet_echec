36a63e6017e2c72aa5338065176a5974018563e2
NotFraym <galois.mathieu02@gmail.com>
2023-10-04 08:54:43 +1100

    Header + fonctionnalités

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
 
-    <h1>Chess Event</h1>
+    <a href="index.php"><h1>Chess Event</h1></a>
     
     <a href="connexion.php">Se connecter</a>
     <a href="inscription.php">S'inscrire</a>
