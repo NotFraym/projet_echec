eaa6c01be7be0a643edbf0ab46acfba522016aed
NotFraym <galois.mathieu02@gmail.com>
2023-10-04 08:43:43 +1100

    Base du site

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
+
 </head>
+
 <body>
 
 <?php include 'header.php'; ?>
     
-</body>
-</html>
 <h2>Bienvenue sur Chess Event !</h2>
 <h3>Sur Chess Event, vous pouvez créer vos propres tournois d'échecs, avec vos propres règles !</h3>
+
+<?php include 'header.php'; ?>
+
+</body>
+</html>
+
