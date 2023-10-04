1e92d07809fc09bb2d93e98ac151e9f45418fd2c
NotFraym <galois.mathieu02@gmail.com>
2023-10-05 10:34:22 +1100

    Gestion Utilisateur

--- connexion.php
+++ connexion.php
@@ -12,6 +12,16 @@
 <body>
 
 <?php include 'header.php'; ?>
+
+<form id="connexion" method="POST" action="traitement_connexion.php">
+        <label for="nom">Nom d'utilisateur :</label>
+        <input type="text" id="nom" name="nom" required>
+        <br>
+        <label for="mdp">Mot de passe :</label>
+        <input type="password" id="mdp" name="mdp"  required>
+        <br>
+        <input type="submit" value="Se connecter" name="ok">
+    </form>
     
 <?php include 'footer.php'; ?>
 
