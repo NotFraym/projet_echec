ca9b29c5ee86273cb3834c9c8ae5e7dc302bb068
NotFraym <galois.mathieu02@gmail.com>
2023-10-05 09:55:18 +1100

    Système Utilisateur

--- connexion.php
+++ connexion.php
@@ -13,34 +13,6 @@
 
 <?php include 'header.php'; ?>
     
-<div id="login">
-
-    <h2>Connexion</h2>
-
-    <form action="traitement_inscription.php" method="POST">
-
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required><br><br>
-            
-        <label for="mdp">Mot de passe :</label>
-        <input type="password" id="mdp" name="mdp" required><br><br>
-              
-        </select><br>
-            
-        <input type="submit" value="Se connecter">
-
-        <?php
-        if(isset($_GET['erreur'])){
-        $err = $_GET['erreur'];
-        if($err==1 || $err==2)
-        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-        }
-        ?>
-
-        </form>
-
-</div>
-
 <?php include 'footer.php'; ?>
 
 </body>
--- inscription.php
+++ inscription.php
@@ -1,42 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-
     <meta charset="UTF-8">
     <link rel="stylesheet" href="inscription_connexion.css" media="screen" type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription - ChessEvent</title>
-
 </head>
-
 <body>
+    <?php include 'header.php'; ?>
 
-<?php include 'header.php'; ?>
+    <div id="messageDiv">
+        <!-- Les messages seront affichés ici -->
+    </div>
 
-<main>
-
-    <h2>Inscription</h2>
-
-    <form action="traitement_inscription.php" method="POST">
-
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required><br><br>
-            
+    <form id="inscriptionForm" method="POST">
+        <label for="nom">Nom d'utilisateur :</label>
+        <input type="text" id="nom" name="nom" required>
+        <br>
         <label for="mdp">Mot de passe :</label>
-        <input type="password" id="mdp" name="mdp" required><br><br>
-            
-        <label for="confirm_mdp">Confirmez le mot de passe :</label>
-        <input type="password" id="confirm_mdp" name="confirm_mdp" required><br><br>
-            
-        </select><br>
-            
-        <input type="submit" value="S'inscrire">
-        
-        </form>
+        <input type="password" id="mdp" name="mdp"  required>
+        <br>
+        <label for="mdp"> Vérification du mot de passe :</label>
+        <input type="password" id="c_mdp" name="c_mdp" required>
+        <br>
+        <input type="submit" value="S'inscrire" name="ok">
+    </form>
 
-    </main>
-   
-<?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
 
-</body>
-</html>
