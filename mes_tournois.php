79c000a0bfb225a84b3bf36f3452c09d453bfef9
NotFraym <galois.mathieu02@gmail.com>
2023-10-12 15:26:12 +1100

    Système création tournoi

--- mes_tournois.php
+++ mes_tournois.php
@@ -1,3 +1,15 @@
-<?php
-include "session_start.php";
-?>
+<?php include "session_start.php"; ?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <?php include "include/head.php" ?>
+    </head>
+    <body>
+        <?php include 'include/header.php'; ?>
+
+        <a href="../nouveau_tournoi.php">Nouveau tournoi</a>
+
+
+        <?php include 'include/footer.php'; ?>
+    </body>
+</html>
