897ce4426d189eddcc05bc4cbc112c18b283d712
NotFraym <galois.mathieu02@gmail.com>
2023-10-17 15:09:53 +1100

    modification page d'accueil

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <?php include "include/head.php"?>
+        <?php include "include/head.php" ?>
     </head>
     <body class="dark-theme">
         <?php include 'include/header.php'; ?>
         <h2>Bienvenue sur ChessQuizz !</h2>
         <h3>Sur ChessQuizz, venez tester vos connaissances du monde des échecs !</h3>
-        <h3>Afin de commencer, veuillez vous connecter, ou créer un compte !</h3>
+        <?php
+        if (isset($_SESSION['user_nom'])) {
+            echo '<h3>Bienvenue, ' . htmlspecialchars($_SESSION['user_nom']) . '</h3>';
+            echo '<h2>Choisissez une catégorie de quiz :</h2>';
+            echo '<ul>';
+            echo '<li><a href="quiz_ouverture.php">Ouvertures</a></li>';
+            echo '<li><a href="quiz_joueurs.php">Joueurs célèbres</a></li>';
+            echo '</ul>';
+        } else {
+            echo '<h3>Afin de commencer, veuillez vous connecter, ou créer un compte !</h3>';
+        }
+        ?>
         <?php include 'include/footer.php'; ?>
     </body>
 </html>
-
