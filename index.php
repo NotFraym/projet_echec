9270e25d313cc91b77197c59f6a4d0dbf7b093f8
NotFraym <galois.mathieu02@gmail.com>
2023-10-17 14:59:36 +1100

    modification index

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <?php include 'include/header.php'; ?>
         <h2>Bienvenue sur ChessQuizz !</h2>
         <h3>Sur ChessQuizz, venez tester vos connaissances du monde des échecs !</h3>
+        <h3>Afin de commencer, veuillez vous connecter, ou créer un compte !</h3>
         <?php include 'include/footer.php'; ?>
     </body>
 </html>
